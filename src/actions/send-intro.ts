61e5c7da96abc321107aea6ca3e69d83f509cdd5
Narottam Singh Bisht <bishtnarottam@gmail.com>
2022-01-26 02:10:42 +0530

    update made according to the update to okcupid site

--- src/actions/send-intro.ts
+++ src/actions/send-intro.ts
@@ -5,21 +5,21 @@ import { sendIntroButtonXpath } from "../utils/xpath";
 export default async function sendIntros(page: Page) {
   try {
     for (let i = 0; i < 100; i++) {
-      await page.waitForSelector("span.cardsummary-realname");
+      await page.waitForSelector("span.card-content-header__text");
       const chickNameElement: ElementHandle<Element> | null = await page.$(
-        "span.cardsummary-realname"
+        "span.card-content-header__text"
       );
       const chickName = await page.evaluate(
         (element) => element.innerText,
         chickNameElement
       );
-      await page.evaluate(() => {
-        const viewProfileSpan = document.getElementsByClassName(
-          "cardsummary-profile-link"
-        );
-        const viewProfile = viewProfileSpan[0].getElementsByTagName("a");
-        viewProfile[0].click();
-      });
+      // await page.evaluate(() => {
+      //   const viewProfileSpan = document.getElementsByClassName(
+      //     "cardsummary-profile-link"
+      //   );
+      //   const viewProfile = viewProfileSpan[0].getElementsByTagName("a");
+      //   viewProfile[0].click();
+      // });
       // await page.click("span.cardsummary-profile-link a", { delay: 2000 }); // view profile link click
       await page.waitForXPath(sendIntroButtonXpath, { visible: true });
       const sendIntroButton = await page.$x(sendIntroButtonXpath);
@@ -27,9 +27,10 @@ export default async function sendIntros(page: Page) {
       await page.waitForSelector("textarea.messenger-composer"); // wait for intro dialog box to open
       await page.type(
         "textarea.messenger-composer",
-        `Hey ${chickName} ${_INTROMESSAGE_}`
+        `Hii ${chickName} ${_INTROMESSAGE_}`
       );
       await page.click("button.messenger-toolbar-send", { delay: 2000 }); // send intro button
+      await page.waitForTimeout(2000);
       await page.evaluate(() => {
         const sendIntroModalCloseButton = document.getElementsByClassName(
           "connection-view-container-close-button"
@@ -37,18 +38,23 @@ export default async function sendIntros(page: Page) {
         (sendIntroModalCloseButton[0] as HTMLButtonElement).click();
       }); // close send-intro dialog box cross button
       console.log("Send Intro to-->", chickName);
-      await page.evaluate(() => {
-        const navbarAnchorElements =
-          document.getElementsByClassName("navbar-link");
-        for (const anchorElement of navbarAnchorElements) {
-          if ((anchorElement as HTMLAnchorElement)?.href.includes("discover")) {
-            (anchorElement as HTMLAnchorElement).click();
-            return;
-          }
-        }
-      });
+      // await page.evaluate(() => {
+      //   const navbarAnchorElements =
+      //     document.getElementsByClassName("navbar-link");
+      //   for (const anchorElement of navbarAnchorElements) {
+      //     if ((anchorElement as HTMLAnchorElement)?.href.includes("discover")) {
+      //       (anchorElement as HTMaLAnchorElement).click();
+      //       return;
+      //     }
+      //   }
+      // });
     }
   } catch (e) {
-    console.error("e", e);
+    console.error("e", e.message);
+    await page.goto("https://okcupid.com", {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
+    await sendIntros(page);
   }
 }
